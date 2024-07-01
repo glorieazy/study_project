e77b4e4ded82ebf95983ae60c80bf35a0092ffd1
Stuhlbeinfuss <Robin.Victor@tu-dortmund.de>
2024-07-01 13:40:52 +0200

    Add files via upload

--- as_main.py
+++ as_main.py
@@ -102,16 +102,17 @@ def main():
     
     # Sort the frames after the numbers
     frames_folder = sorted(frames_folder)
-    '''half_frames = len(frames_folder)/2
-
-    primary_frames_folder = frames_folder[half_frames:]
-    primary_frames_folder = sorted(primary_frames_folder, key=extract_number)
-    secondary_frames_folder = frames_folder[:half_frames]
-    secondary_frames_folder = sorted(secondary_frames_folder, key=extract_number)
-
-    frames_folder_iter = iter(frames_folder)'''
 
     length_frames_folder = len(frames_folder)
+
+    dir_path = './frames'
+    number = 0
+    # Iterate directory
+    for path in os.listdir(dir_path):
+        # check if current path is a file
+        if os.path.isfile(os.path.join(dir_path, path)):
+            if 'jonas' in path:
+                number += 1
 
     landmark_positioning_nose_1 = np.zeros((2,length_frames_folder),dtype = float)
     landmark_positioning_nose_2 = np.zeros((2,length_frames_folder/2),dtype = float)
@@ -127,9 +128,12 @@ def main():
 
     landmark_positioning_right_ankle_1 = np.zeros((2,length_frames_folder),dtype = float)
     landmark_positioning_right_ankle_2 = np.zeros((2,length_frames_folder/2),dtype = float)
-
+    mark1=0 
+    highest_hand1=0
+    mark2=0
+    highest_hand2=0
     # Go through frames of one video and put landmark positions in an array
-    for index in range(0,length_frames_folder/2):
+    for index in range(0,number):
 
         #frames_folder_iter = iter(frames_folder)
 
@@ -142,7 +146,7 @@ def main():
                 image = frames_folder[index]
                 current_frames_path = os.path.join(frames_folder_path, image) 
                 current_image = cv2.imread(current_frames_path)
-                if index == length_frames_folder/2-1:
+                if index == number-1:
                     print('detecting bad frames done')
                     break
                 next_frame = frames_folder[index+1]
@@ -276,7 +280,9 @@ def main():
                     dx = right_wrist_landmark_1.x - right_wrist_landmark_2.x
                     dy = right_wrist_landmark_1.y - right_wrist_landmark_2.y  
                     counter = 0
-                    
+                    if right_wrist_landmark_1.x < highest_hand1 :
+                        highest_hand1 = right_wrist_landmark_1
+                        mark1 = index
                     # Check if landmarks move unrealisticly much
                     while dx > 0.05 | dx < -0.05 | dy > 0.05 | dy < -0.05 :
                         counter = counter + 1
@@ -401,6 +407,8 @@ def main():
                     print('No right ankle detected')                                        
 
                     
+    #time synchronizing (code here isnt working yet, neeeds support for secondary video in detecting bad frames)
+
 
 
 
@@ -411,7 +419,7 @@ def main():
 
 
 
-    for index in range(length_frames_folder/2,length_frames_folder):
+    for index in range(number,length_frames_folder):
 
         #frames_folder_iter = iter(frames_folder)
 
@@ -558,7 +566,9 @@ def main():
                     dx = right_wrist_landmark_1.x - right_wrist_landmark_2.x
                     dy = right_wrist_landmark_1.y - right_wrist_landmark_2.y  
                     counter = 0
-                    
+                    if right_wrist_landmark_1.x < highest_hand2 :
+                        highest_hand2 = right_wrist_landmark_1
+                        mark2 = index - number
                     # Check if landmarks move unrealisticly much
                     while dx > 0.05 | dx < -0.05 | dy > 0.05 | dy < -0.05 :
                         counter = counter + 1
@@ -683,12 +693,73 @@ def main():
                     print('No right ankle detected')                                        
 
 
+    #cutting frames at the front so that the moment for synchronizing is at the same frame number (cutting frames isnt working yet)
+    if mark1 > mark2:
+        for i in range (0, mark1-mark2):
+            #name of the frames from primary video
+            file_path = './frames_annotated/Daria_forhand_frame_' + str(i) + '.jpg'
+            os.remove(file_path)
+            length_video1 = length_video1 - mark1 +mark2 
+    else:
+        for index in range (0, mark2-mark1):
+            #name of the frames from secindary video
+            file_path = './frames_annotated/Daria_forhand_frame_' + str(i) + '.jpg'
+            os.remove(file_path)
+        #cut frames from video 2 at start amount of frames needed to be cut: mark2-mak1
+
+        #updating video length
+        length_video2 = length_video2 - mark2 + mark1
+
+
+    #cutting frames at the end so that both video have same length
+    if length_video1 > length_video2:
+        for i in range (length_video2, length_video1):
+            #name of the frames from primary video
+            file_path = './frames_annotated/Daria_forhand_frame_' + str(i) + '.jpg'
+            os.remove(file_path)
+    else:
+        #cut frames video2 at end with number higher length_video1
+        for i in range (length_video1, length_video2):
+            #name of the frames from secondary video
+            file_path = './frames_annotated/Daria_forhand_frame_' + str(i) + '.jpg'
+            os.remove(file_path)
+
+
+    # Folders containing relevant images
+    frames_folder_path = 'frames'
+
+    # Get lists of image filenames from folder
+    frames_folder = [filename for filename in os.listdir(frames_folder_path) if filename.endswith(('.jpg', '.png', '.jpeg'))]
+    
+    # Function for extracting the number from a frame
+    def extract_number(frame):
+       return int(''.join(filter(str.isdigit, frame)))
+    
+    # Sort the frames after the numbers
+    frames_folder = sorted(frames_folder)
+
+    length_frames_folder = len(frames_folder)        
+
 
     landmark_error = np.zeros((length_frames_folder/2))
 
+    if mark1 > mark2:
+        start1 = mark1-mark2
+    else:
+        start1 = 0
+    start2 = length_video1 + abs(mark1-mark2)
+        
+    if length_video1 > length_video2:
+        end1 = length_video2 + start1
+        end2 = length_video1 + length_video2 + abs(mark1-mark2)
+    else:
+        end1 = length_video1 + start1
+        end2 = length_video1 + length_video1 + abs(mark1-mark2)
+
+
     for j in range(0,length_frames_folder/2):
-        x_offset = landmark_positioning_nose_1[0,j] - landmark_positioning_nose_1[0,length_frames_folder/2+1]
-        y_offset = landmark_positioning_nose_1[1,j] - landmark_positioning_nose_1[1,length_frames_folder/2+1]
+        x_offset = landmark_positioning_nose_1[0,start1+j] - landmark_positioning_nose_1[0,start2+j]
+        y_offset = landmark_positioning_nose_1[1,start1+j] - landmark_positioning_nose_1[1,start2+j]
 
         image = frames_folder[j]
         current_frames_path = os.path.join(frames_folder_path, image) 
@@ -704,7 +775,6 @@ def main():
             x_error = landmark1.x - landmark2.x - x_offset
             y_error = landmark1.y - landmark2.y - y_offset
             landmark_error[j] = norm([x_error, y_error]) ** 2
-            
 
 
 
@@ -712,41 +782,6 @@ def main():
 
 
 
-'''
 
-    for index, image in enumerate(frames_folder):
 
-        with mp_pose.Pose(
-            static_image_mode=True,
-            model_complexity=2,
-            enable_segmentation=True,
-            min_detection_confidence=0.5) as pose:
-                current_frames_path = os.path.join(frames_folder_path, image) 
-                current_image = cv2.imread(current_frames_path)
-                next_frame = next(frames_folder,'end')
-                next_frames_path = os.path.join(frames_folder_path,next_frame)
 
-                if next_frames_path == 'end':
-                    print('detecting bad frames done')
-                else:
-                    next_image = cv2.imread(next_frames_path)
-
-                    # Convert the BGR image to RGB before processing
-                    results_1 = pose.process(cv2.cvtColor(current_image, cv2.COLOR_BGR2RGB))
-                    results_2 = pose.process(cv2.cvtColor(next_image, cv2.COLOR_BGR2RGB))
-
-                    nose_landmark_1 = results_1.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE] if results_1.pose_landmarks else None
-                    nose_landmark_2 = results_2.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE] if results_2.pose_landmarks else None
-
-                    # Check if nose landmarks are detected in both images
-                    if nose_landmark_1 and nose_landmark_2:
-                        dx = nose_landmark_1.x - nose_landmark_2.x
-                        dy = nose_landmark_1.y - nose_landmark_2.y  
-                        while dx > 0.05 | dx < -0.05 | dy > 0.05 | dy < -0.05 :
-                            print('bad pose detection in' + next_frames_path)
-                            next_frame = next(next_frame)
-                            next_frames_path = os.path.join(frames_folder_path,next(image,'end'))
-                    else:
-                        print('no nose detected')
-                             
-                    '''
